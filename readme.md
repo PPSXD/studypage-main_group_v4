34f335a57a616a81eb48174562449c0ac24a7962
PPSXD <raynerchung888@gmail.com>
2024-11-11 20:38:53 +0800

    first commit

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 Homepage
 
 https://ppsxd.github.io/studypage-main_group_v4/
+
+20241111
+updated style.css
+
+20241111
+updated script_service.js
+
+20241111
+updated boat.html boat.css script_boat.js
--- script/script_service.js
+++ script/script_service.js
@@ -1,85 +1,36 @@
-/* document.getElementById('test-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    // Get the input values
-    const dj_number = document.getElementById('dj').value;
-
-    // Display the values
-    console.log('DJ number:', dj_number);
-}); */
-
-// $(document).ready(function() {
-//     $('#dj-form').on('submit', function(event) {
-//         event.preventDefault(); // Prevent the default form submission
-
-//         // Get the input values
-//         let dj_number = $('#dj').val();
-
-//         dj_number = dj_number * 5;
-//         // Display the values
-//         console.log('DJ number:', dj_number);
-//     });
-
-//     $('#toy-form').on('submit', function(event) {
-//         event.preventDefault(); // Prevent the default form submission
-
-//         // Get the input values
-//         let toy_number = $('#toy').val();
-
-//         toy_number = toy_number * 3;
-
-//         // Display the values
-//         console.log('toy number:', toy_number);
-//     });
-// });
-
-// fetchAndFindClass('../en/invoice.html', 'cart-items');
-// async function fetchAndFindClass(url, className) {
-//   try {
-//    // Fetch the external HTML file
-//    const response = await fetch("file:///C:/Users/Billy/Desktop/study/class/en/invoice.html");
-//    const text = await response.text();
-//    const parser = new DOMParser();
-//    const doc = parser.parseFromString(text, 'text/html');
-
-//    // Use querySelector to find the element with the specified class
-//    const element = doc.querySelector(`.${className}`);
-//    console.log(element);
-//    } catch (error) {
-//      console.error('Error fetching or parsing the HTML file:', error);
-//    }
-// }
-
+// Define service product list
 const products = [
-    { name: "DJ onboard", price: 2500, id: 1, quantity: 1,},
-    { name: "water-toy", price: 1200, id: 2, quantity: 1,},
-    { name: "catering-onboard", price: 1500, id: 3, quantity: 1, },
-    { name: "asian-style", price: 400, id: 4, quantity: 1, },
-    { name: "mediterranean-style", price: 500, id: 5, quantity: 1, },
-    { name: "continental-style", price: 550, id: 6, quantity: 1,},
+    { id: 1, name: "DJ onboard",           price: 2500, quantity: 0},
+    { id: 2, name: "water-toy",            price: 1200, quantity: 0},
+    { id: 3, name: "catering-onboard",     price: 1500, quantity: 0},
+    { id: 4, name: "asian-style",          price: 400,  quantity: 0},
+    { id: 5, name: "mediterranean-style",  price: 500,  quantity: 0},
+    { id: 6, name: "continental-style",    price: 550,  quantity: 0},
   ];
-  
+
+// Init a cart list
 let cart = [];
 
-// const productsHTML = products.map(
-//     (product) => `<h2 class="product-name">${product.name}</h2><strong>$${product.price}</strong>
-//                   <button class="service-button" id=${product.id}>Add to Cart</button>`);
-
+// Prepare the price, addCart button, deleteItem, quantity, addItem for service HTML
 const productsHTML = products.map(
-    (product) => `<strong>$${product.price}</strong>
-                  <button class="service-button" id=${product.id}>Add to Cart</button>
-                  <div class="mid-${product.id} unit">
-                    <button class ="unit-button" onclick={decrItem(${product.id})}>-</button>
-                    <p>${product.quantity}</p>
-                    <button class ="unit-button" onclick={incrItem(${product.id})}>+</button>
-                  </div>`);
+      (product) => `<strong>$${product.price}</strong>
+                    <button class="service-button" id=${product.id}>Add to Cart</button>
+                    <div class="mid-${product.id} unit">
+                      <button class ="unit-button" onclick={deleteItem(${product.id})}>-</button>
+                      <p>${product.quantity}</p>
+                      <button class ="unit-button" onclick={addItem(${product.id})}>+</button>
+                    </div>`);
 
+// Insert the prepared price, addCart button, deleteItem, quantity, addItem to service HTML
 for (let i = 0; i <= productsHTML.length-1; i++) {
-  const queryname = `.service-${products[i].id}`;
-  const result = document.querySelector(queryname);
+  const productName = `.service-${products[i].id}`;
+  const result = document.querySelector(productName);
   result.innerHTML = productsHTML[i];
 }
 
+// ======= Event Listener =======
+
+// Add eventListener on all addCart button
 let num = document.querySelectorAll(".service-button").length;
 for (let i = 0; i < num; i++) {
   document.querySelectorAll(".service-button")[i].addEventListener("click", (e) => {
@@ -87,95 +38,172 @@ for (let i = 0; i < num; i++) {
   });
 }
 
-
+// Add eventListener on payment reset button
 document.querySelector(".payment-button-reset").addEventListener("click", (e) => {
+  // reset the cart list to empty list
   cart = [];
+
+  // reset the quantity to unit 0
   for (let i = 1; i <= products.length; i++) {
     const temp = document.querySelector(".mid-" + i + " p");
-    temp.innerText = 1;
+    temp.innerText = 0;
   }
+
+  // Update the cart and the total amount
   updateCart();
   getTotal(cart);
 });
 
-window.addEventListener('beforeunload', function (event) {
-  cart = [];
-  for (let i = 1; i <= products.length; i++) {
-    const temp = document.querySelector(".mid-" + i + " p");
-    temp.innerText = 1;
-  }
-  updateCart();
-  getTotal(cart);
-  console.log('Page is about to be reloaded or closed');
+// Reset all the value in the cart list if the page is reloaded even though item is added in the cart list below 
+window.addEventListener('load', (e) => {
+  console.log("here load");
+  console.log(cart);
+  console.log(products);
+  // for (let i = 1; i <= products.length; i++) {
+  //   const temp = document.querySelector(".mid-" + i + " p");
+  //   //console.log(products[i-1]);
+  //   temp.innerText = products[i-1].quantity;
+  // }
+  // updateCart();
+  // getTotal(cart);
 });
+
+
+// ======= Function =======
 
 function addToCart(products, id){
   const product = products.find((product) => product.id === id);
   const cartProduct = cart.find((product) => product.id === id);
+
+  // If cart list is not empty, add cartProduct in existing cart list
   if (cartProduct != undefined && product.id == cartProduct.id) {
-    //  console.log("addtocart-incrItem");
-    incrItem(id);
-  } else {
-    //  console.log("addtocart-input item");
-    //  console.log("product - " + product.name + product.price + product.id + product.quantity);
+    addItem(id);
+  } 
+  // else cart list is empty, add item in the cart list
+  else {
+    //  console.log("product - " + product.name + " " + product.price + " " + product.id + " " +  product.quantity);
     cart.unshift(product);
-    //  console.log("cart - " + cart[0].price);
+    const temp = document.querySelector(".mid-" + id + " p");
+    cart[0].quantity = 1;
+    temp.innerText = cart[0].quantity;
+    // console.log(temp)
+    // console.log("cart - " + cart[0].price + " " + cart[0].id);
   }
+
+  // Update the cart and the total amount
   updateCart();
   getTotal(cart);
 };
 
-function incrItem(id) {
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i] && cart[i].id == id) {
-      cart[i].quantity += 1;
-      const temp = document.querySelector(".mid-" + id + " p");
-      temp.innerText = cart[i].quantity;
+function addItem(id) {
+  // console.log(cart);
+  const product = products.find((product) => product.id === id);
+  const cartProduct = cart.find((product) => product.id === id);
+  // console.log(cartProduct);
+  // ===========================
+  // If cart list is not empty, add cartProduct in existing cart list
+  if (cartProduct != undefined && product.id == cartProduct.id) {
+    console.log("add here");
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i] && cart[i].id == id) {
+        cart[i].quantity += 1;
+        const temp = document.querySelector(".mid-" + id + " p");
+        temp.innerText = cart[i].quantity;
+      }
     }
+  } 
+  // else cart list is empty, add item in the cart list
+  else {
+    console.log("add here 2")
+  //   //  console.log("product - " + product.name + " " + product.price + " " + product.id + " " +  product.quantity);
+    cart.unshift(product);
+    const temp = document.querySelector(".mid-" + id + " p");
+    cart[0].quantity = 1;
+    temp.innerText = cart[0].quantity;
+  // console.log(temp)
+  // console.log("cart - " + cart[0].price + " " + cart[0].id);
   }
-  updateCart();
-  getTotal(cart);
-}
+  // =============================
 
-function decrItem(id) {
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].id == id && cart[i].quantity > 1) {
-      cart[i].quantity -= 1;
-      const temp = document.querySelector(".mid-" + id + " p");
-      temp.innerText = cart[i].quantity;
-    }
-  }
+  // for (let i = 0; i < cart.length; i++) {
+  // if (cart[i] && cart[i].id == id) {
+  // cart[i].quantity += 1;
+  // const temp = document.querySelector(".mid-" + id + " p");
+  // temp.innerText = cart[i].quantity;
+  // }
+  // }
   updateCart();
   getTotal(cart);
 }
 
 function deleteItem(id) {
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].id === id) {
-      cart[i].quantity = 1;
-      cart.splice(i, 1);
+  // console.log(cart);
+  const product = products.find((product) => product.id === id);
+  const cartProduct = cart.find((product) => product.id === id);
+  // console.log(cartProduct);
+  // ===========================
+  // If cart list is not empty, delete cartProduct in existing cart list
+  if (cartProduct != undefined && product.id == cartProduct.id) {
+    console.log("delete here");
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].id == id && cart[i].quantity > 1) {
+        console.log("delete here if");
+        cart[i].quantity -= 1;
+        const temp = document.querySelector(".mid-" + id + " p");
+        temp.innerText = cart[i].quantity;
+        console.log(cart[i]);
+      }
+      else {
+        console.log("delete here else");
+        const removeProductById = (array, id) => { return array.filter(product => product.id != id);};
+        console.log(cart);
+        console.log(id);
+        const updateProduct = removeProductById(cart, id);
+        cart = updateProduct;
+        const temp = document.querySelector(".mid-" + id + " p");
+        temp.innerText = 0;
+        console.log(updateProduct);
+        console.log(cart);
+      }
     }
-  }
+  } 
+  // else cart list is empty, add item in the cart list
+  else {
+    console.log("delete here2");
+    //   //  console.log("product - " + product.name + " " + product.price + " " + product.id + " " +  product.quantity);
+      // cart.unshift(product);
+      // const temp = document.querySelector(".mid-" + id + " p");
+      // cart[0].quantity = 1;
+      // temp.innerText = cart[0].quantity;
+    // console.log(temp)
+    // console.log("cart - " + cart[0].price + " " + cart[0].id);
+    }
+    // =============================
+  
+    // for (let i = 0; i < cart.length; i++) {
+    // if (cart[i] && cart[i].id == id) {
+    // cart[i].quantity += 1;
+    // const temp = document.querySelector(".mid-" + id + " p");
+    // temp.innerText = cart[i].quantity;
+    // }
+    // }
   updateCart();
   getTotal(cart);
 }
 
-// const cartHTML = cart.map(
-//   (item) => `<div class="cart-item">
-//               <h3>${item.name}</h3>
-//               <div class="cart-detail">
-//                 <div class="mid">
-//                   <button onclick={decrItem(${item.id})}>-</button>
-//                   <p>${item.quantity}</p>
-//                   <button onclick={incrItem(${item.id})}>+</button>
-//                 </div>
-//                 <p>$${item.price}</p>
-//                 <button onclick={deleteItem(${item.id})} class="cart-product" id=${item.id}>D</button>
-//               </div>
-//             </div>`
-// );
+// function deleteItem(id) {  
+//   for (let i = 0; i < cart.length; i++) {
+//     if (cart[i].id === id) {
+//       cart[i].quantity = 1;
+//       cart.splice(i, 1);
+//     }
+//   }
+//   updateCart();
+//   getTotal(cart);
+// }
 
 function updateCart() {
+  // Prepare the invoice item for invoice HTML 
   const cartHTML = cart.map(
     (item) => `<div class="cart-item">
                 <h3>${item.name}</h3>                
@@ -183,30 +211,37 @@ function updateCart() {
                 <p class="amount">$${item.price}</p>                                
               </div>`
   );
+
   console.log("cartHTML - " + cartHTML);
+  // Update the invoice item list and temporary store in the invoice-paid variable for invoice HTML 
   localStorage.setItem('invoice-paid', cartHTML);
-  //  const cartItems = document.querySelector(".cart-items");
-  //  cartItems.innerHTML = cartHTML.join("");
 }
 
 function getTotal(cart) {
   console.log(cart);
+
+  // Update totalItem (all product) and each product (total quantity and price)
   let { totalItem, cartTotal } = cart.reduce(
     (total, cartItem) => {
-      console.log(cartItem);
       console.log(total);
+      console.log(cartItem);
       total.cartTotal += cartItem.price * cartItem.quantity;
       total.totalItem += cartItem.quantity;
       return total;
     },
     { totalItem: 0, cartTotal: 0 }
   );
-  console.log("noOfItem - " + `${totalItem} items`);
+
+  console.log(totalItem);
+  // console.log("noOfItem - " + `${totalItem} items`);
+
+  // Update the totalItem and temporary store in the noOfItem variable for invoice HTML 
   localStorage.setItem('noOfItem', `${totalItem} items`);
-  //  const totalItemsHTML = document.querySelector(".noOfItems");
-  //  totalItemsHTML.innerHTML = `${totalItem} items`;
+
   console.log("total - " + `$${cartTotal}`);
+  //  const totalItemsHTML = document.querySelector(".noOfItems");
+  //  totalItemsHTML.innerHTML = `${totalItem} items`;  
+  
+  // Update the total and temporary store in the total variable for invoice HTML 
   localStorage.setItem('total', `$${cartTotal}`);
-  //  const totalAmountHTML = document.querySelector(".total");
-  //  totalAmountHTML.innerHTML = `$${cartTotal}`;
 }
