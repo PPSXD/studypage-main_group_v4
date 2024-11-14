6855d567a80c5758b90071b3ac391a9bcc075099
PPSXD <raynerchung888@gmail.com>
2024-11-14 22:06:47 +0800

    first commit

--- readme.md
+++ readme.md
@@ -1,8 +1,5 @@
 Homepage
 
-<<<<<<< HEAD
-https://ppsxd.github.io/studypage-main_group_v4/
-=======
 https://ppsxd.github.io/studypage-main_group_v4/
 
 20241111
@@ -23,4 +20,3 @@ updated script_service.js
 
 20241111
 updated boat.html boat.css script_boat.js
->>>>>>> 520335741d053e9fc5caef1ce25bbf203d26367f
--- script/app.js
+++ script/app.js
@@ -5,12 +5,12 @@ navBtn_1.addEventListener("click", () => {
     navLinks_1.classList.toggle("active");
 });
 
-const navBtn_2 = document.getElementById("nav-language-toggle");
-const navLinks_2 = document.getElementById("language-nav-list");
+// const navBtn_2 = document.getElementById("nav-language-toggle");
+// const navLinks_2 = document.getElementById("language-nav-list");
 
-navBtn_2.addEventListener("click", () => {
-    navLinks_2.classList.toggle("active");
-});
+// navBtn_2.addEventListener("click", () => {
+    // navLinks_2.classList.toggle("active");
+// });
 
 // add date
 const date = document.getElementById("date");
